44918ed94e41e2214e45ef0182bebe02a6b55426
antoine <antoineroth657@gmail.com>
2020-05-28 17:40:17 +0200

    ajout des forge-apis et création d'un fichier de variables globales

---
+++ pas_fini/app.config.js
@@ -0,0 +1,22 @@
+// Document contenant les variables globales nécessaires
+// pour l'instantiation d'un viewer forge
+//
+// A changer dès que vous changerez d'application (hébergée sur le serveur de forge)
+
+// L'id de votre application (inchangeable)
+CLIENT_ID = "UUU6OanaZPbMtx2KEdS1IAucKGxF5jMy";
+
+// Le mot de passe pour accéder à l'application (changeable à volonté)
+CLIENT_SECRET = "I5e736a84b8014e4";
+
+// le token demandé à la connexion avec le service forge (nécessaire pour 
+// toutes les interractions avec l'api forge autre que l'authentification)
+ACCESS_TOKEN = null;
+
+// Le nom de la session dans laquelle vous êtes (les fichiers accessibles 
+// cahngeront d'une session à l'autre)
+BUCKET_NAME = null;
+
+// Le code spécial correspondant au fichier à visualiser
+// ATTENTION : bien enlever le = à la fin de l'urn fournit (si il y en a un)
+URN = null;
--- pas_fini/viewer_controller.js
+++ pas_fini/viewer_controller.js
@@ -1,1 +1,105 @@
-a
+// le token et l'urn permettent l'affichage 
+// dans le viewer de la ressource correspondante (à l'urn demandé)
+var token = 'eyJhbGciOiJIUzI1NiIsImtpZCI6Imp3dF9zeW1tZXRyaWNfa2V5In0.eyJzY29wZSI6WyJkYXRhOnJlYWQiLCJkYXRhOndyaXRlIiwiZGF0YTpjcmVhdGUiLCJidWNrZXQ6cmVhZCIsImJ1Y2tldDpjcmVhdGUiXSwiY2xpZW50X2lkIjoiVVVVNk9hbmFaUGJNdHgyS0VkUzFJQXVjS0d4RjVqTXkiLCJhdWQiOiJodHRwczovL2F1dG9kZXNrLmNvbS9hdWQvand0ZXhwNjAiLCJqdGkiOiJFTHUzZzdVM2pGaUJpT2NuMDZlQmx6S1VTam1qVDBtVzdtZUUweUZNNjZ4Zk9VZXVkbWpab0g3SFdPa2NmTE1JIiwiZXhwIjoxNTkwNjc2Nzg1fQ.PMMMVBuoGuMxgZvHR4ik59FNMdbSTj4lT9IH8Gc0YiI';
+var urn = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dGVzdF9jb2xsYWJfdjQvUHJvamVjdDEuaWZj'
+
+function getToken (CLIENT_ID, CLIENT_SECRET) {
+  var ForgeSDK = require('forge-apis')
+
+  // autoRefresh est à true si on veut que ACCESS_TOKEN 
+  // se rafraichisse automatiquement, false sinon 
+  var autoRefresh = true;
+
+  var oAuth2TwoLegged = new ForgeSDK.AuthClientTwoLegged(CLIENT_ID, CLIENT_SECRET, [
+      'data:read', 'data:write'
+    ]
+
+    )
+}
+
+// options nécessaires pour le viewer forge
+var initOptions = {
+  documentId: urn,
+  env: 'AutodeskProduction',
+  getAccessToken: function(onGetAccessToken) {
+    return token
+  }
+}
+
+// typically getAccessToken would look like below
+// where /api/forge/token is a REST endpoint
+// on your own server that return a valid forge token
+//
+//getAccessToken: function(onGetAccessToken) {
+//  $.get('/api/forge/token', function(token) {
+//    onGetAccessToken(token.access_token)
+//  })
+//}
+
+/////////////////////////////////////////////////////////////////
+// Document Loaded Handler
+//
+/////////////////////////////////////////////////////////////////
+function onDocumentLoaded (doc) {
+
+  var rootItem = doc.getRootItem()
+
+  // Grab all geometry items
+  var geometryItems =
+    Autodesk.Viewing.Document.getSubItemsWithProperties(
+      rootItem, { 'type': 'geometry' }, true)
+
+  // Pick the first item by default
+  var selectedItem = geometryItems[0]
+
+  var domContainer = document.getElementById('viewer')
+
+  // UI-less Version: viewer without controls and commands
+  //var viewer = new Autodesk.Viewing.Viewer3D(domContainer)
+
+  // GUI Version: viewer with controls
+  var viewer = new Autodesk.Viewing.Private.GuiViewer3D(domContainer)
+
+  viewer.initialize()
+
+  viewer.loadModel(doc.getViewablePath(selectedItem))
+
+viewer.impl.setSelectionColor(new THREE.Color(1,0,0));
+viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, ()=>viewer.select(1))
+}
+
+/////////////////////////////////////////////////////////////////
+// Environment Initialized Handler
+//
+/////////////////////////////////////////////////////////////////
+function onEnvInitialized () {
+
+  Autodesk.Viewing.Document.load(
+    initOptions.documentId,
+    function(doc) {
+      onDocumentLoaded (doc)
+    },
+    function (errCode){
+      onLoadError (errCode)
+    })
+}
+
+/////////////////////////////////////////////////////////////////
+// Error Handler
+//
+/////////////////////////////////////////////////////////////////
+function onLoadError (errCode) {
+
+  console.log('Error loading document: ' + errCode)
+}
+
+/////////////////////////////////////////////////////////////////
+// Bootstraping
+//
+/////////////////////////////////////////////////////////////////
+Autodesk.Viewing.Initializer(
+  initOptions,
+  function() {
+    onEnvInitialized ()
+  }
+)
